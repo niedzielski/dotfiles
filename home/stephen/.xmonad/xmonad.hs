bfd6550d769b2722141dc5c102b035ef36dead1d
Stephen Niedzielski <stephen@niedzielski.com>
2012-08-07 21:20:48 -0600

    Use smart borders. Float full screen windows.

--- home/stephen/.xmonad/xmonad.hs
+++ home/stephen/.xmonad/xmonad.hs
@@ -2,13 +2,19 @@ import XMonad
 import XMonad.Actions.NoBorders
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
 
 myKeys =
     [ ((mod4Mask, xK_g), withFocused toggleBorder) -- Toggle window focus border.
     , ((mod4Mask, xK_p), spawn "dmenu_run")
     ]
 
-main = do
-    xmonad $ gnomeConfig {
-        modMask = mod4Mask -- Use super instead of alt.
+main = xmonad $ gnomeConfig
+    { modMask = mod4Mask -- Use super instead of alt.
+    , layoutHook = smartBorders $ layoutHook gnomeConfig -- Auto border.
+    , manageHook = composeAll
+        [ manageHook gnomeConfig
+        , isFullscreen --> doFullFloat -- When window is supposed to be
+        ]                              -- full screen, make it so.
     } `additionalKeys` myKeys
