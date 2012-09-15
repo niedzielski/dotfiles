68d2083ebac826538102bd8df8c06c120eec1a7c
Stephen Niedzielski <stephen@niedzielski.com>
2012-09-15 13:14:12 -0600

    Centralize modMask preference.

--- home/stephen/.xmonad/xmonad.hs
+++ home/stephen/.xmonad/xmonad.hs
@@ -6,17 +6,18 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Actions.CycleWS
 
+myModMask = mod4Mask
 myKeys =
-    [ ((mod4Mask, xK_g), withFocused toggleBorder) -- Toggle window focus border.
-    , ((mod4Mask, xK_p), spawn "dmenu_run -i")
-    , ((mod4Mask, xK_Left), prevWS)
-    , ((mod4Mask, xK_Right), nextWS)
-    , ((mod4Mask .|. shiftMask, xK_Left), shiftToPrev)
-    , ((mod4Mask .|. shiftMask, xK_Right), shiftToNext)
+    [ ((myModMask, xK_g), withFocused toggleBorder) -- Toggle window focus border.
+    , ((myModMask, xK_p), spawn "dmenu_run -i")
+    , ((myModMask, xK_Left), prevWS)
+    , ((myModMask, xK_Right), nextWS)
+    , ((myModMask .|. shiftMask, xK_Left), shiftToPrev)
+    , ((myModMask .|. shiftMask, xK_Right), shiftToNext)
     ]
 
 main = xmonad $ gnomeConfig
-    { modMask = mod4Mask -- Use super instead of alt.
+    { modMask = myModMask -- Use super instead of alt.
     , layoutHook = smartBorders $ layoutHook gnomeConfig -- Auto border.
     , manageHook = composeAll
         [ manageHook gnomeConfig
