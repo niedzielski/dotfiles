1d711a2b14a0e27f2a7adc6ca02ac4e2756a47ed
Stephen Niedzielski <stephen@niedzielski.com>
2015-04-24 16:53:58 -0600

    Add Basic Fish Completion for Simple Alias Scripts

--- home/stephen/.config/fish/config.fish
+++ home/stephen/.config/fish/config.fish
@@ -7,3 +7,13 @@
 function fish_prompt -d "Prints the command prompt."
   prompt $status $COLUMNS
 end
+
+complete -c chromium --wraps chromium-browser
+complete -c f --wraps find
+complete -c g --wraps grep
+complete -c gradlew --wraps gradle
+complete -c l --wraps ls
+complete -c map --wraps x
+complete -c s --wraps sed
+complete -c v --wraps gvim
+complete -c x --wraps xargs
